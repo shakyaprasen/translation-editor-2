1b26f150f665802896d5ce8cf6f3ed4d367609ce
shakyaprasen <kaji.prasen@gmail.com>
2020-03-28 14:00:15 +0545

    Change logic for showing translated text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // Import React dependencies.
 import React, { useCallback, useMemo, useState, useEffect } from "react";
 // Import the Slate editor factory.
-import { createEditor, Transforms } from 'slate'
+import { Element, Editor, createEditor, Transforms } from 'slate'
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react'
 
 import { convert } from './romanizedNepali'; 
+
 
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -14,7 +15,7 @@ const App = () => {
   const { isVoid } = editor;
 
   editor.isVoid = element => {
-    return element.type === 'readOnly' ? true : isVoid(element);
+    return element.void ? true : isVoid(element);
   };
   const [translations, setTranslations] = useState({});
   // Add the initial value when setting up our state.
@@ -22,6 +23,7 @@ const App = () => {
     {
       paragraphRef: 1,
       type: 'readOnly',
+      void: true,
       children: [{ text: 'A line of text in a paragraph.' }],
     },
     {
@@ -30,13 +32,28 @@ const App = () => {
       children: [{ text: '' }],
     },
     {
+      paragraphRef: 1,
+      type: 'translatedTextPreview',
+      translatedText: '',
+      void: true,
+      children: [{ text: '' }],
+    },
+    {
       paragraphRef: 2,
       type: 'readOnly',
+      void: true,
       children: [{ text: 'A line of text in a paragraph.' }],
     },
     {
       paragraphRef: 2,
       type: 'translation',
+      children: [{ text: '' }],
+    },
+    {
+      paragraphRef: 2,
+      type: 'translatedTextPreview',
+      void: true,
+      translatedText: '',
       children: [{ text: '' }],
     },
   ]);
@@ -59,40 +76,38 @@ const App = () => {
     switch (props.element.type) {
       case 'readOnly':
         return <ReadOnlyElement {...props} />
+      case 'translatedTextPreview':
+        return <TranslatedText {...props} />
       default:
         return <DefaultElement {...props} />
     }
   }; 
 
-  const clickTranslationBtn = (ref, editor) => {
-    const translatedValue =  translations[ref];
-    console.log(translatedValue);
-    // Transforms.delete(
-    //   editor,
-    //   { at: editor.selection, unit: 'line' }
-    // );
-    // Transforms.insertText(
-    //   editor,
-    //   translatedValue,
-    //   { match: n => n.paragraphRef === ref && n.type === "translation" }
-    // );
-  };
 
   const DefaultElement = props => {
     return (
       <div>
         <p {...props.attributes}>{props.children}</p>
-        <div style={{ position: 'relative', top: 10 }}>
-          <p>
-            &nbsp;{translations[props.element.paragraphRef]}
-          </p>
-          <button onClick={e => clickTranslationBtn(props.element.paragraphRef, editor)} type="button" style={translationMenu}>Use Translation</button>
-        </div>
       </div>
     )
   }
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+    <Slate 
+      editor={editor} 
+      value={value} 
+      onChange={value => {
+        const translations = value.filter(textBlock => textBlock.type === "translation");
+        const changedValue = value.map(textBlock => {
+          if(textBlock.type === "translatedTextPreview") {
+            const text = translations.find(block => block.paragraphRef === textBlock.paragraphRef).children[0].text;
+            return { ...textBlock, translatedText: convertSentences(text) };
+          }
+          return textBlock;
+        });
+        console.log(changedValue);
+        setValue(changedValue);
+      }}
+    >
       <Editable 
         renderElement={renderElement}
       />
@@ -103,15 +118,18 @@ const App = () => {
 
 const ReadOnlyElement = props => {
   return (
-    <div {...props.attributes} style={{ position: 'relative' }}>
+    <div {...props.attributes} >
       <p contentEditable={false}>{props.element.children[0].text}</p>{props.children}
     </div>
   )
 }
-export default App;
 
-var translationMenu = {
-  position: 'absolute',
-  top: -25,
-  left: 0,
-};
+const TranslatedText = props => {
+  return (
+    <div {...props.attributes} >
+      <p contentEditable={false}>{props.element.translatedText ? props.element.translatedText : ' '}</p>{props.children}
+    </div>
+  )
+}
+
+export default App;
