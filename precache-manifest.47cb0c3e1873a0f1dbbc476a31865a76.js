383cfc5e31fa338b5f2f531003b904982f690429
shakyaprasen <kaji.prasen@gmail.com>
2020-03-31 08:03:16 +0545

    Updates

---
+++ precache-manifest.47cb0c3e1873a0f1dbbc476a31865a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8501305bb750ee8df156b470b4f185e9",
+    "url": "/translation-editor-2/index.html"
+  },
+  {
+    "revision": "339a2915849d6395aaaa",
+    "url": "/translation-editor-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bd09449e980f4bcba499",
+    "url": "/translation-editor-2/static/js/2.1f1ab0ac.chunk.js"
+  },
+  {
+    "revision": "ebe3e3146ec8811972f3f01e6a145274",
+    "url": "/translation-editor-2/static/js/2.1f1ab0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339a2915849d6395aaaa",
+    "url": "/translation-editor-2/static/js/main.c833613d.chunk.js"
+  },
+  {
+    "revision": "c552f438fdf2bbe2a65b",
+    "url": "/translation-editor-2/static/js/runtime-main.b6ef4191.js"
+  }
+]);
