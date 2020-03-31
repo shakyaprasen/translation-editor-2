40db80d6831696892eae477532da0780e46e1a34
shakyaprasen <kaji.prasen@gmail.com>
2020-03-31 10:00:50 +0545

    Added tailwind, postcss and purgecss

--- src/App.js
+++ src/App.js
@@ -107,6 +107,12 @@ const App = () => {
         setValue(changedValue);
       }}
     >
+      <button
+        onClick={e => console.log('clicked')}
+        className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
+      >
+        Testing Button
+      </button>
       <Editable 
         renderElement={renderElement}
       />
